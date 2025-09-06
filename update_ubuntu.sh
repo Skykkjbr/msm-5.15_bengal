eaf96d56771fa11830008ca752245078b7b3ce20
skykkjbr <gregorioraphael540@gmail.com>
2025-09-06 17:45:23 -0300

    kernel: update_ubuntu.sh

---
+++ update_ubuntu.sh
@@ -0,0 +1,21 @@
+sudo apt install figlet
+
+#Instalando pacotes
+figlet Instalando pacotes
+sudo apt update
+sudo apt upgrade
+sudo apt install repo
+sudo apt install git
+sudo apt install rsync
+sudo apt install zip unzip
+sudo apt install openssl
+sudo apt install gnutls-bin
+sudo apt install make
+sudo apt update
+sudo apt upgrade
+
+#logar na conta
+figlet sincroniando conta
+git config --global user.email "gregorioraphael540@gmail.com"
+git config --global user.name "skykkjbr"
+

a54f84e52f831a13cc8152e983886d55920d8baa
skykkj <gregorioraphael540@gmail.com>
2025-09-06 15:40:21 -0300

    kernel: initial kernel configs and username builder info

---
+++ update.sh
@@ -0,0 +1,18 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install repo
+sudo apt install git
+sudo apt install openssl
+sudo apt zip unzip
+sudo apt rsync
+sudo apt bc make curl wget
+sudo apt install gnutls-bin
+sudo apt install make
+sudo apt update
+sudo apt upgrade
+
+
+git config --global user.email "gregorioraphael540@gmail.com"
+git config --global user.name "skykkjbr"
+
+
